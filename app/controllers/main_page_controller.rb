fab87b62e66b1d4a9d12685e8dc0eb06efd604ab
gusvicuna <gpvicuna@miuandes.cl>
2018-04-13 12:22:02 -0300

    Created the main page

---
+++ app/controllers/main_page_controller.rb
@@ -0,0 +1,6 @@
+class MainPageController < ApplicationController
+
+  def home
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   resources :profiles
   resources :projects
   resources :users
+
+  root 'main_page#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
